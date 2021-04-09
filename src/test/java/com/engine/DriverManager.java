b92ae462e456f06f73113f0f5c11f09499a33603
Saikat B <saikat2406@gmail.com>
2021-04-09 01:34:02 +0100

    changes

--- src/test/java/com/engine/DriverManager.java
+++ src/test/java/com/engine/DriverManager.java
@@ -13,12 +13,12 @@ public class DriverManager {
         if(driver==null){
             switch (environmentMap.get("Browser")){
                 case "chrome":
-                    System.out.println("Starting Chrome Browser");
+                    System.out.println("Starting Chrome Browser...");
                     System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\chromedriver.exe");
                     driver = new ChromeDriver();
                     break;
                 case "edge":
-                    System.out.println("Starting Edge Browser");
+                    System.out.println("Starting Edge Browser...");
                     System.setProperty("webdriver.edge.driver", "C:\\Softwares\\msedgedriver.exe");
                     driver = new EdgeDriver();
                     break;
